fd7994c87275eb9d303c969c062fb974ae77d480
dyadyasawa <dyadyasawa@gmail.com>
2024-08-11 10:29:32 +0300

    Админка для модели Manufacturer.

--- manufacturers/admin.py
+++ manufacturers/admin.py
@@ -1,3 +1,15 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from manufacturers.models import Manufacturer
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+    )
+    list_filter = ("name",)
+    search_fields = ("name",)
+
